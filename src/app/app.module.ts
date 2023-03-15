7ff6e61091725ba5e6c94de1bf31fa5aeb442697
ChadiSM <csalama@uoc.edu>
2023-03-15 14:51:40 +0100

    Actualizacion 1
    eliminar archivos innecesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XxxComponent } from './xxx/xxx.component';
+
 import { SongComponent } from './song/song.component';
 import { NavComponent } from './nav/nav.component';
-import { MusicPlayerComponent } from './music-player/music-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XxxComponent,
+
     SongComponent,
     NavComponent,
-    MusicPlayerComponent
+
   ],
   imports: [
     BrowserModule,
